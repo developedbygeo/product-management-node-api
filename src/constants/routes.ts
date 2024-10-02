db031e450864ac6b7138bd0491a639620bd801d3
developedbygeo <georgiosanagnostou@hotmail.com>
2024-10-02 11:03:02 +0200

    chore: adding PROTECTED_ROUTES

---
+++ src/constants/routes.ts
@@ -0,0 +1,18 @@
+export const ROUTES_ROOT = {
+    /* Auth */
+    API: '/api',
+};
+
+export const PROTECTED_ROUTES = {
+    /* Products */
+    ALL_PRODUCTS: '/products',
+    SPECIFIC_PRODUCT: '/product/:id',
+
+    /* Updates */
+    ALL_UPDATES: '/updates',
+    SPECIFIC_UPDATE: '/update/:id',
+
+    /* Update Points */
+    ALL_UPDATE_POINTS: '/update-points',
+    SPECIFIC_UPDATE_POINT: '/update-point/:id',
+} as const;
