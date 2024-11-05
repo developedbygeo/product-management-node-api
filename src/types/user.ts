f7223fd0c23b9cd02fe531fbf5ecbbfb487a437a
developedbygeo <georgiosanagnostou@hotmail.com>
2024-11-06 01:44:49 +0200

    feat: extend user request types for user creation and sign-in

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,6 +1,22 @@
 import jwt from 'jsonwebtoken';
 import { Request } from 'express';
+import { User } from '@prisma/client';
 
 export type RequestWithUser = Request & {
     user?: string | jwt.JwtPayload;
+};
+
+export type CreateUserRequest = Omit<Request, 'body'> & {
+    body: {
+        username: User['username'];
+        email: User['email'];
+        password: User['password'];
+    };
+};
+
+export type SignInUserRequest = Omit<Request, 'body'> & {
+    body: {
+        email: User['email'];
+        password: User['password'];
+    };
 };
