464fa964c42ba55c92aa0154f301802b84ccca0c
developedbygeo <georgiosanagnostou@hotmail.com>
2024-12-03 02:35:14 +0200

    chore: cleanup

--- src/router.ts
+++ src/router.ts
@@ -1,70 +1,13 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { PROTECTED_ROUTES } from './constants/routes';
-import { body, oneOf, validationResult } from 'express-validator';
-import {
-    handleValidationErrors,
-    validateRequiredName,
-    validatePutSpecificProduct,
-    validatePostUpdate,
-    validatePutUpdatePoint,
-    validatePostUpdatePoint,
-    validatePostSpecificProduct,
-    validatePutSpecificUpdate,
-} from './middleware/validators';
-import { getProducts, getSpecificProduct } from './handlers/product';
+import productRouter from './routes/products';
+import updateRouter from './routes/updates';
+import updatePointRouter from './routes/updatePoint';
 
 const router = Router();
 
-/* ----------------- Products ----------------- */
-
-router.get(PROTECTED_ROUTES.ALL_PRODUCTS, getProducts);
-router.get(PROTECTED_ROUTES.SPECIFIC_PRODUCT, getSpecificProduct);
-router.put(
-    PROTECTED_ROUTES.SPECIFIC_PRODUCT,
-    validatePutSpecificProduct,
-    (req: Request, res: Response) => {
-        res.json({ message: 'Hello' });
-    }
-);
-router.post(
-    PROTECTED_ROUTES.ALL_PRODUCTS,
-    validatePostSpecificProduct,
-    (req: Request, res: Response) => {}
-);
-router.delete(PROTECTED_ROUTES.SPECIFIC_PRODUCT, () => {});
-
-/* ----------------- Updates ----------------- */
-router.get(PROTECTED_ROUTES.ALL_UPDATES, (req: Request, res: Response) => {});
-router.get(PROTECTED_ROUTES.SPECIFIC_UPDATE, () => {});
-router.put(
-    PROTECTED_ROUTES.SPECIFIC_UPDATE,
-    validatePutSpecificUpdate,
-    (req: Request, res: Response) => {
-        res.json({ message: 'Hello' });
-    }
-);
-router.post(
-    PROTECTED_ROUTES.ALL_UPDATES,
-    validatePostUpdate,
-    (req: Request, res: Response) => {
-        res.json({ message: 'Hello' });
-    }
-);
-router.delete(PROTECTED_ROUTES.SPECIFIC_PRODUCT, () => {});
-
-/* ----------------- Update Points ----------------- */
-router.get(PROTECTED_ROUTES.ALL_UPDATE_POINTS, () => {});
-router.get(PROTECTED_ROUTES.SPECIFIC_UPDATE_POINT, () => {});
-router.put(
-    PROTECTED_ROUTES.SPECIFIC_UPDATE_POINT,
-    validatePutUpdatePoint,
-    (req: Request, res: Response) => {}
-);
-router.post(
-    PROTECTED_ROUTES.ALL_UPDATE_POINTS,
-    validatePostUpdatePoint,
-    (req: Request, res: Response) => {}
-);
-router.delete(PROTECTED_ROUTES.SPECIFIC_UPDATE_POINT, () => {});
+router.use(PROTECTED_ROUTES.ALL_PRODUCTS, productRouter);
+router.use(PROTECTED_ROUTES.ALL_UPDATES, updateRouter);
+router.use(PROTECTED_ROUTES.ALL_UPDATE_POINTS, updatePointRouter);
 
 export default router;
