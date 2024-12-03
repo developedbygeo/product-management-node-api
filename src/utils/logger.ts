279c096a240f03526ba3d4678555b762f934a045
developedbygeo <georgiosanagnostou@hotmail.com>
2024-12-03 02:30:48 +0200

    chore: enhance logger configuration with improved error formatting and logging levels

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,7 +5,7 @@ const logPath = resolve(__dirname, '../../logs/app.log');
 
 const loggerLevels = {
     ...winston.config.syslog.levels,
-    http: 0,
+    http: 6,
 };
 
 const loggerColors = {
@@ -20,10 +20,20 @@ export const logger = winston.createLogger({
 
     format: winston.format.combine(
         winston.format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
+        winston.format.errors({ stack: true }),
         winston.format.json()
     ),
     transports: [
-        new winston.transports.Console(),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.printf((info) => {
+                    return `${info.timestamp} [${info.level}]: ${info.message} ${
+                        info.stack || ''
+                    }`;
+                })
+            ),
+        }),
         new winston.transports.File({ filename: logPath }),
     ],
 });
